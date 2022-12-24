2d735b8cead278522fb0af66f40148fe69dadab8
amirabotaref95 <amir.abotaref@runnableweb.com>
2022-12-24 18:04:37 +0200

    adding new

---
+++ index.js
@@ -0,0 +1,47 @@
+import cors from "cors";
+import express from "express";
+import http from "http";
+// import mongoose from "mongoose";
+import path from "path";
+import { Server } from "socket.io";
+import { fileURLToPath } from "url";
+// import router from "./api/routes.js";
+// import sockets from "./socket/sockets.js";
+
+// await mongoose.connect(
+//   "mongodb+srv://bitfumes123:bitfumes123@cluster0.yk0jpvz.mongodb.net/?retryWrites=true&w=majority"
+// );
+
+const app = express();
+const PORT = 4000;
+
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: ["http://localhost:3000"],
+  },
+});
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// app.use(cors());
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+  //   res.json({ test: "test" });
+});
+// app.use("/", router);
+
+io.on("connection", (socket) => {
+  //console.log("Connection is ready");
+  socket.on("send-code", (data) => {
+    socket.emit("code-from-server", data);
+  });
+  socket.on("disconnect", (socket) => {
+    console.log("user left");
+  });
+});
+
+httpServer.listen(PORT, () => {
+  console.log("Server is running at http://localhost:4000");
+});
