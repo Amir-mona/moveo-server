94bbbde3be92fcf1a30b8c6098ca72d71a2e2516
amirabotaref95 <amir.abotaref@runnableweb.com>
2022-12-24 23:53:46 +0200

    finsh project

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = 4000;
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["https://amir-moveoproject.netlify.app"],
   },
 });
 
